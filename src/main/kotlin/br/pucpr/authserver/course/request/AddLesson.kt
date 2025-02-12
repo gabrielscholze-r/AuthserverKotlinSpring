5f3f39cac3876f300a3ee09c5f68c74db390f522
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-02-11 22:01:00 -0300

    🔧

---
+++ src/main/kotlin/br/pucpr/authserver/course/request/AddLesson.kt
@@ -0,0 +1,20 @@
+package br.pucpr.authserver.course.request
+
+
+import org.jetbrains.annotations.NotNull
+
+data class AddLesson(
+    @field:NotNull
+    val name: String="",
+
+    @field:NotNull
+    val duration: Int,
+
+    @field:NotNull
+    val course: Long
+) {
+
+
+
+
+}
